b5ef916568bae099eb139fcf775023b44890e987
Oliver <Oliver@OLITOWER-PC>
2016-07-22 18:35:56 +0100

    First Exercise completed

---
+++ ProjectEulerConsole/ProjectEulerConsole/Exercises.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerConsole
+{
+    class Exercises
+    {
+
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+        /// The sum of these multiples is 23.
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        public static double ExerciseOne(int maxNum)
+        {
+            List<double> numberList = new List<double>();
+            for (int i = 0; i < maxNum; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    numberList.Add(i);
+                }
+
+            }
+            double result = numberList.Sum();
+            return result;
+        }
+    }
+}
---
+++ ProjectEulerConsole/ProjectEulerConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool anotherTest = false;
+            do
+            {
+                TestSelector();
+                Console.Write("would you like to run another test? (y): ");
+                if (Console.ReadLine() == "y")
+                {
+                    anotherTest = true;
+                }
+            }
+            while (anotherTest);
+        }
+        static void TestSelector()
+        {
+            Console.Write("Select test number: ");
+            string responseString = Console.ReadLine().Trim();
+            int responseInt;
+            if (!int.TryParse(responseString, out responseInt))
+            {
+                return;
+            }
+            else { Tests(responseInt); }
+        }
+        static void Tests(int response)
+        {
+            switch (response)
+            {
+                case 1:
+                    Console.Write("enter number lof natural numbers to go through: ");
+                    int maxNum;
+                    if (int.TryParse(Console.ReadLine(), out maxNum))
+                    {
+                        double result = Exercises.ExerciseOne(maxNum);
+                        Console.WriteLine("sum of multiples of 3's & 5's = " + result);
+                    }
+                    break;
+            }
+        }
+    }
+}
